a56260d95cea2ba214edb40466b197bf37a73101
innovationitdev <omarfaruk@innovationit.com.bd>
2023-01-25 23:58:26 +0600

    initial commit

---
+++ part6/app.js
@@ -0,0 +1,17 @@
+var app = Vue.createApp({
+    data() {
+        return {
+            count: 0
+        };
+    },
+
+    methods: {
+        increment() {
+            this.count++
+        },
+        decrement() {
+            this.count--
+        }
+    }
+});
+app.mount("#app");
