f75bbc6d1bb111ef928166521b2cf9a568c8c824
innovationitdev <omarfaruk@innovationit.com.bd>
2023-01-26 14:57:55 +0600

    part 7 end

--- part7/app.js
+++ part7/app.js
@@ -10,6 +10,19 @@ const app = Vue.createApp({
             // console.log(e.target.value)
             this.name = e.target.value
 
+        },
+        handelFormSubmit() {
+            console.log('form submited');
+            // e.preventDefault();
+        },
+        handelCardClick(e) {
+            console.log(e);
+            console.log("card click");
+        },
+        handelViewClick(e) {
+            console.log(e);
+            // e.stopPropagation();
+            console.log("view click");
         }
     }
 });
