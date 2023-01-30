30497b5ff3ee55f7924cc4e3d553a854662acb7a
innovationitdev <omarfaruk@innovationit.com.bd>
2023-01-30 22:31:51 +0600

    part 10 done

---
+++ part10/app.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            bgColor: "blue",
+            textColor: "red"
+        };
+    },
+
+    methods: {
+
+    }
+});
+app.mount("#app");
